0ecd533445f98001a39449f98b5e116f78621315
IchikaCoding <193454050+IchikaCoding@users.noreply.github.com>
2025-12-31 01:12:15 +0900

    WIP(script-v2): displayLog関数のコメントを整理し、startTimers関数を追加

--- script-v2.js
+++ script-v2.js
@@ -2,22 +2,52 @@ console.log("Hello World");
 
 // 3秒後にログを表示する処理
 // 0秒後にログを表示する処理
-function displayLog() {
-  setTimeout(() => {
-    console.log("3秒後バージョン");
-  }, 3000);
+// function displayLog() {
+//   setTimeout(() => {
+//     console.log("3秒後バージョン");
+//   }, 3000);
 
-  setTimeout(() => {
-    console.log("0秒後バージョン");
-  }, 0);
-}
+//   setTimeout(() => {
+//     console.log("0秒後バージョン");
+//   }, 0);
+// }
 
-// 値を作る：setTimeOutのidを習得する
-// 状態管理：setTimeOutのidを返す処理
-// 値を表示：setTimeOutでログを表示する処理
+// 値を作る：setTimeoutのidを習得する
+// 状態管理：setTimeoutのidを返す処理
+// 値を表示：setTimeoutでログを表示する処理
 
 /** 工程で分離させて3行メモ（最小でOK）
  * 1) タイマー開始: setTimeout を呼び、timeoutId を取得する
  * 2) IDの保持: timeoutId を変数に保存しておく（あとでキャンセルするため）
- * 3) ログ出力: 発火したらログを出す。キャンセルしたらキャンセルログを出す
+ * 3) ログ出力: 発火したらtextで受け取った引数をdisplayLog() でログに出す。キャンセルしたらキャンセルログを出す
  */
+
+/**
+ * タイマー開始: setTimeout を呼び、timeoutId を取得する
+ * @param {string} displayText
+ * @param {number} ms
+ * @returns {number} timeoutId
+ */
+function startTimers(displayText, ms) {
+  const timeoutId = setTimeout(() => {
+    displayLog(displayText);
+  }, ms);
+  return timeoutId;
+}
+
+/**
+ * ログ出力: 発火したらtextで受け取った引数をdisplayLog() でログに出す処理
+ * @param {string} text
+ */
+function displayLog(text) {
+  console.log(text);
+}
+
+/**
+ * idを受け取ってsetTimeoutをキャンセル。キャンセルのログも表示する。
+ * @param {number} id
+ */
+function clearTimer(id) {
+  clearTimeout(id);
+  displayLog("setTimeoutはキャンセルされました");
+}
