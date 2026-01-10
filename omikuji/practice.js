67729f2958de9b4f466ecb38e45b9229b6e1b6cc
IchikaCoding <IchikaCoding@users.noreply.github.com>
2026-01-10 22:33:02 +0900

    refactor(practice): omikujiFuncの関数分割に成功✨️✨️

--- omikuji/practice.js
+++ omikuji/practice.js
@@ -6,11 +6,12 @@ const resultElement = document.getElementById("result");
 // The color APIから取得したデータを表示する部分
 const colorInfoElement = document.getElementById("color-info");
 const loadingElement = document.getElementById("loading");
-
+// ボタンのアニメーションを追加
+const containerElement = document.querySelector(".container");
 // イベントを追加
 // omikujiBtnElementを押したら，おみくじのロジックが動くイベント
 
-omikujiBtnElement.addEventListener("click", omikujiFunc);
+omikujiBtnElement.addEventListener("click", handleOmikuji);
 
 // omikujiFuncという関数を定義
 // ボタン押されたらsetTimeoutで3秒間待つ（画面には「占い中・・・」を表示）
@@ -18,66 +19,124 @@ omikujiBtnElement.addEventListener("click", omikujiFunc);
 // 配列から，インデック番号がランダムで選ばれる
 // その選ばれた配列のなかの文字列がresultElementに表示される
 
-async function omikujiFunc() {
+// TODO イベントハンドラーらしくhandleOmikujiという名前に変える
+async function handleOmikuji() {
+  setLoadingUI();
+
+  try {
+    const waitPromise = waitThreeSeconds();
+    const colorObject = fetchLuckyColor();
+    // 3秒だけ待つ処理なので変数宣言なし
+    await waitPromise;
+    // ! 分割代入？
+    // カラーの名前とカラーコードのオブジェクトを作成
+    const { colorName, colorCode } = await colorObject;
+    const result = getRandomFortune();
+    showFortuneResult(result);
+    showColorInfo(colorName, colorCode);
+  } catch (error) {
+    console.error(error);
+    showColorError();
+  } finally {
+    resetUI();
+  }
+}
+
+/**
+ * ローディング中のぐるぐるを表示する関数
+ */
+function setLoadingUI() {
   resultElement.style.color = "#d4af37";
-  const waitString = "占い中・・・";
+
   omikujiBtnElement.disabled = true;
-  resultElement.textContent = waitString;
-  console.log(waitString);
+  resultElement.textContent = "占い中・・・";
+
   // loadingElement要素のhiddenプロパティにfalseを代入→ぐるぐる表示が見えるようにする
   loadingElement.hidden = false;
 
-  // ボタンのアニメーションを追加
-  const containerElement = document.querySelector(".container");
   containerElement.classList.add("shake-animation");
+}
 
-  // TODO: ここの処理が途中
-  const waitPromise = new Promise((resolve) => {
+/**
+ *　setTimeoutによって3秒後にresolveする関数
+ * @returns {Promise} 3秒間待つ
+ */
+async function waitThreeSeconds() {
+  return new Promise((resolve) => {
     setTimeout(() => {
       resolve("3秒の待ち時間終了");
     }, 3000);
   });
-  console.log(waitPromise);
-  // 3秒間待ち時間が終わったらアニメーションは削除する
-  containerElement.classList.remove("shake-animation");
+}
+
+/**
+ * ラッキーカラーを取得する関数
+ * TODO JSDocの書き方怪しい！！
+ * @returns {{colorName:string, colorCode:string}}
+ */
+async function fetchLuckyColor() {
+  // hexの16進数の0からffffff（白）までのランダムな数字を生成
+  // 0x は「これから書く数は16進数だよ」という接頭辞
+  // 0b は 2 進数、0o は 8 進数の接頭辞
+  const hex = Math.floor(Math.random() * 0xffffff)
+    .toString(16)
+    .padStart(6, "0");
+  const res = await fetch(`https://www.thecolorapi.com/id?hex=${hex}`);
+  if (!res.ok) {
+    throw new Error("APIエラー");
+  }
+  const data = await res.json();
+  // dataから使いたいデータ(名前、カラーコード)を変数に代入
+  return {
+    colorName: data.name?.value ?? "カラーネームなし",
+    colorCode: data.hex?.value ?? `#${hex}`,
+  };
+}
+/**
+ *  おみくじの中身を作る
+ * TODO JSDocの書き方配列バージョンわからない！
+ * @return {Array}
+ */
+function getRandomFortune() {
   const messageArray = ["大吉", "中吉", "笑吉", "いちかどん吉"];
   const randomNumIndex = Math.floor(Math.random() * messageArray.length);
-  console.log({ randomNumIndex });
-  const result = messageArray[randomNumIndex];
+  return messageArray[randomNumIndex];
+}
+/**
+ * おみくじの結果を画面に表示する関数
+ * @param {string} result
+ */
+function showFortuneResult(result) {
   resultElement.textContent = result;
   console.log(result);
-  if (result === "いちかどん吉") {
-    resultElement.style.color = "#ff0000";
-  } else {
-    resultElement.style.color = "#d4af37";
-  }
-  // pokeAPIからデータ取得して、エラーになったら投げる
-  // resをJSのオブジェクトに戻すとどうしてポケモンのデータになるの？
+  resultElement.style.color = result === "いちかどん吉" ? "#ff0000" : "#d4af37";
+}
 
-  try {
-    // hexの16進数の0からffffff（白）までのランダムな数字を生成
-    // 0x は「これから書く数は16進数だよ」という接頭辞
-    // 0b は 2 進数、0o は 8 進数の接頭辞
-    const hex = Math.floor(Math.random() * 0xffffff)
-      .toString(16)
-      .padStart(6, "0");
-    const res = await fetch(`https://www.thecolorapi.com/id?hex=${hex}`);
-    if (!res.ok) {
-      throw new Error("APIエラー");
-    }
-    const data = await res.json();
-    console.log(data);
-    // dataから使いたいデータ(名前、カラーコード)を変数に代入
-    const colorName = data.name?.value ?? "カラーネームなし";
-    const colorCode = data.hex?.value ?? `#${hex}`;
-    // その変数をcolorInfoElementに代入する
-    colorInfoElement.textContent = `あなたのラッキーカラー：${colorName}(${colorCode})`;
-    colorInfoElement.style.color = colorCode;
-  } catch (error) {
-    console.error(error);
-    console.error("ラッキーカラー取得できず・・・");
-  } finally {
-    loadingElement.hidden = true;
-    omikujiBtnElement.disabled = false;
-  }
+/**
+ *
+ * @param {string} colorName
+ * @param {string} colorCode
+ */
+function showColorInfo(colorName, colorCode) {
+  // その変数をcolorInfoElementに代入する
+  colorInfoElement.textContent = `あなたのラッキーカラー：${colorName}(${colorCode})`;
+  colorInfoElement.style.color = colorCode;
+}
+/**
+ * ラッキーカラーのエラーを画面に表示する関数
+ */
+function showColorError() {
+  colorInfoElement.textContent = "ラッキーカラー取得できず・・・";
+}
+
+/**
+ * 画面のリセットをする関数
+ */
+function resetUI() {
+  // ぐるぐるを消す
+  loadingElement.hidden = true;
+  // ボタンの無効化を解除する
+  omikujiBtnElement.disabled = false;
+  // 3秒間待ち時間が終わったらアニメーションは削除する
+  containerElement.classList.remove("shake-animation");
 }
